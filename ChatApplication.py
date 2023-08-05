57fe8dfded61e4964b4dd8d2cc3c43d1ed1b912a
RANSFORD OPPONG <faradayfoundationgh@gmail.com>
2023-08-05 07:53:53 +0000

    chatApp and voiceAssistant
    completion

--- ChatApplication.py
+++ ChatApplication.py
@@ -1,7 +1,8 @@
 #GPT-3 powered AGI Chat Application: RANSFORD OPPONG: Aug 4,2023
 import os
 import openai
-openai.api_key = "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"
+import gradio as gr
+openai.api_key = "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"
 #command to tell the model how to arrange the inputs and outputs.
 start_sequence = "\nAI:"
 restart_sequence = "\Human: "
@@ -18,8 +19,31 @@ def gpt_output(prompt):
         presence_penalty = 0.6,
         stop = ["Human: ","AI: "]
     )
-    print("AI response: ",response.choice[0].text)
+    return response.choice[0].text
 #a loop to take input from a user when the function is true
-while True:
-    query = input("Ask a QUestion to AI:\n")
-    gpt_output(query)
+# while True:
+#     query = input("Ask a QUestion to AI:\n")
+#     gpt_output(query)
+
+#context storage or history
+def chatgpt_clone(input,history):
+    history = history or []
+    s = list(sum(history,()))
+    s.append(input)
+    inp = ''.join(s)
+    output = gpt_output(inp)
+    history.append((input,output))
+    return history,history
+#pip install gradio - chat application web interface
+block = gr.Blocks()
+#builtin gradio functions for the interface
+with block:
+    gr.Markdown("""<h1><center>Rans AI Assistant</center></h1>""")
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder = prompt)
+    state = gr.State()
+    # session = gr.File()
+    submit = gr.Button("SEND")
+    submit.click(chatgpt_clone,inputs=[message,state],outputs=[chatbot,state])
+#set the launch to true
+block.launch(debug=True)
